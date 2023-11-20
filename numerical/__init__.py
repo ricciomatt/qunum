649e152e4d14b950c79e4fabd459b6bc795ee118
Matthew Riccio <riccio_matt@outlook.com>
2023-11-20 00:49:00 -0600

    Update Structure

--- numerical/__init__.py
+++ numerical/__init__.py
@@ -1,9 +1,8 @@
 from . import seml
-from . import const_and_mat as const
+from . import constants as const
 from . import lattice_operators as ops
 from . import algebra
 from . import seml
 from . import lattice_operators
-from . import const_and_mat 
 from . import physics as phys
 from .physics import quantum_computing as qpid
