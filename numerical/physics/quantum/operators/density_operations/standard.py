3eea0b2e31a2cf16ad68198ad79a8990a6ed64e6
Matthew Riccio <riccio_matt@outlook.com>
2023-11-24 19:23:44 -0600

    Deprecated avoid

--- numerical/physics/quantum/operators/density_operations/standard.py
+++ numerical/physics/quantum/operators/density_operations/standard.py
@@ -7,7 +7,7 @@ import torch
 def get_cols(ix):
     return f"column_{str(ix)}"
 vgc = np.vectorize(get_cols)
-
+'''
 @nb.jit(parallel = True, fastmath = True)
 def ptrace_np_ix(ix:NDArray[np.int64], p:NDArray)->NDArray:
     pA = NDArray.zeros(ix.shape[0], ix.shape[0], dtype = p.dtype)
@@ -15,6 +15,7 @@ def ptrace_np_ix(ix:NDArray[np.int64], p:NDArray)->NDArray:
         for j in nb.prange(ix.shape[0]):
             pA[i,j] = p[ix[i], ix[j]].sum()
     return pA
+'''
 
 @torch.jit.script
 def ptrace_torch_ix(ix:torch.Tensor, p:torch.Tensor)->torch.Tensor:
