17d6110690579cac9fe68129a98260f1d6b1953f
Matthew Riccio <riccio_matt@outlook.com>
2023-11-28 16:44:17 -0600

    Fix Hilbert

--- symbolic/physics/quantum/operations/sqobj_ops/d_prod_ops.py
+++ symbolic/physics/quantum/operations/sqobj_ops/d_prod_ops.py
@@ -4,8 +4,8 @@ from ...qobjs.sqobj import SQobj
 def direct_prod(A:SQobj, B:SQobj):
     if(not isinstance(A,SQobj) or not isinstance(B,SQobj)):
         raise TypeError('Must Be SQobj type')
-    if(A._metadata.hilber_space_dims == B._metadata.hilber_space_dims):
+    if(A._metadata.hilbert_space_dims == B._metadata.hilbert_space_dims):
         return SQobj(dprod(A,B), n_particles=A._metadata.n_particles+B._metadata.n_particles, 
-                     hilbert_space_dims=A._metadata.hilber_space_dims)
+                     hilbert_space_dims=A._metadata.hilbert_space_dims)
     else:
         raise ValueError('Hilbert Space Dimensions must match')
