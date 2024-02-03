Fixing the Derivative Operator and more

qunum/numerical/physics/pinns/networks/nuetrino.py
@@ -2,19 +2,14 @@ from typing import Any
 import torch
 from torch.nn import Module, Linear, Sigmoid, Tanh, Softmax, Conv1d, Sequential, ReLU, LeakyReLU as LeLU
 from torch import Tensor
-from ....mathematics.algebra.representations import su
-from ....mathematics.algebra import commutator as comm
-from ...quantum.qobjs.torch_qobj import TQobj, direct_prod as dp_
 from torch.linalg import matrix_exp as expm
 from typing import Callable
-
-class TimeEvolutionNuetrino:
+from ....mathematics import einsum
+class TimeEvolutionNuetrino(Module):
     def __init__(self, 
-                 H:Tensor,
-                 num_layers:int,
-                 dims:int = 2,)->None:
-        
-        
+                 H:Tensor
+                 )->None:
+        super(TimeEvolutionNuetrino, self).__init__()
         self.GammaReal = Sequential(
             Linear(1, 48),
             LeLU(),
@@ -35,6 +30,9 @@ class TimeEvolutionNuetrino:
             LeLU(),
             
             Linear(128, 48),
+            LeLU(),
+            
+            Linear(48, H.shape[0])
             
         )
         self.GammaImag = Sequential(
@@ -57,6 +55,9 @@ class TimeEvolutionNuetrino:
             LeLU(),
             
             Linear(128, 48),
+            LeLU(),
+            
+            Linear(48, H.shape[0])
             
         )
         self.HBasis = H
@@ -65,11 +66,8 @@ class TimeEvolutionNuetrino:
     def __call__(self, x:Tensor)->Tensor:
         R = self.GammaReal(x.real)
         I = self.GammaImag(x.real)
-        Gamma = R + 1j*I
-        U = self.HBasis[0] * Gamma[0]
-        for h in range(1, self.HBasis.shape[0]):
-            U += self.HBasis[h]*Gamma[h]
-        return expm(U)
+        Gamma = torch.complex(R, I)
+        return (einsum('bij, Ab->Aij', self.HBasis, Gamma)).expm()
     
     def forward(self, x:Tensor)->Tensor:
         return self.__call__(x)